06e55d722a44467204d67124929f43ee62da3523
jfox <atljeremy@me.com>
2014-01-03 16:02:40 -0500

    Updated photos layout

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -13,55 +13,6 @@ class PhotosController < ApplicationController
   def show
   end
 
-  # GET /photos/new
-  def new
-    @photo = Photo.new
-  end
-
-  # GET /photos/1/edit
-  def edit
-  end
-
-  # POST /photos
-  # POST /photos.json
-  def create
-    @photo = Photo.new(photo_params)
-
-    respond_to do |format|
-      if @photo.save
-        format.html { redirect_to @photo, notice: 'Photo was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @photo }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @photo.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /photos/1
-  # PATCH/PUT /photos/1.json
-  def update
-    respond_to do |format|
-      if @photo.update(photo_params)
-        format.html { redirect_to @photo, notice: 'Photo was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @photo.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /photos/1
-  # DELETE /photos/1.json
-  def destroy
-    @photo.destroy
-    respond_to do |format|
-      format.html { redirect_to photos_url }
-      format.json { head :no_content }
-    end
-  end
-
   # POST /photos/add.json
   def add_photos
     photos = params[:photos]
