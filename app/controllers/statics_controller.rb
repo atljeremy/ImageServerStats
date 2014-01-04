f0c031f3e790ddf4d8de8898c31287e7ac467d9a
Jeremy Fox <atljeremy@me.com>
2014-01-04 14:44:44 -0500

    Added graph to home page

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,6 +1,55 @@
 class StaticsController < ApplicationController
+  require 'gruff'
 
   def index
+
+    @photos = Photo.all.order('download_time DESC')
+
+    if @photos.size > 0
+      g = Gruff::Pie.new
+      g.theme = {
+          :colors => [
+              '#a9dada', # blue
+              '#aedaa9', # green
+              '#daaea9', # peach
+              '#dadaa9', # yellow
+              '#a9a9da', # dk purple
+              '#daaeda', # purple
+              '#dadada' # grey
+          ],
+          :marker_color => '#aea9a9', # Grey
+          :font_color => 'black',
+          :background_colors => '#f8f8f9'
+      }
+      g.title = 'Overall Download Times'
+      data1 = []
+      data2 = []
+      data3 = []
+      data4 = []
+      @photos.each { |photo|
+        case photo.download_time
+          when 0.0..0.3
+            data1 << photo.download_time
+          when 0.4..0.7
+            data2 << photo.download_time
+          when 0.7..1.0
+            data3 << photo.download_time
+          else
+            data4 << photo.download_time
+        end
+      }
+      g.data('0.0-0.3 Seconds', data1) if data1.size > 0
+      g.data('0.4-0.7 Seconds', data2) if data2.size > 0
+      g.data('0.7-1.0 Seconds', data3) if data3.size > 0
+      g.data('1.0+ Seconds', data4) if data4.size > 0
+      filename = 'overall_downloads.png'
+      g.write(filename)
+      path = Rails.root + filename
+      File.open(path, 'rb') {|file|
+        AWS::S3::S3Object.store(filename, file.read, ENV['AWS_BUCKET'], :access => :public_read)
+      }
+      @graph_url = AWS::S3::S3Object.url_for(filename, ENV['AWS_BUCKET'], :authenticated => false)
+    end
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140103212213) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "photos", force: true do |t|
     t.boolean  "cache_hit"
     t.float    "download_time"
